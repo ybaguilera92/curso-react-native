93399dac0220109d9d685363c28187ca8b3ab8cb
yoel.benedico <yoel.benedico@gmail.com>
2025-05-30 13:23:21 -0400

    Update dependecias

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>Hola mundo2</Text>
       <StatusBar style="auto" />
     </View>
   );
